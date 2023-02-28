7c86e508e189454d32c337425bafb62b024bf31d
kawinkumar25 <123533543+kawinkumar25@users.noreply.github.com>
2023-02-28 14:30:07 +0530

    Update Studenttest.java

--- src/main/java/org/example/Studenttest.java
+++ src/main/java/org/example/Studenttest.java
@@ -3,84 +3,48 @@ package org.example;
 import java.util.ArrayList;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.logging.*;
 
 
-class Student {
-    private String name;
-    private int age;
-    private double gpa;
 
-    Student(String name,int age,double gpa)
-    {
-        set(name,age,gpa);
-    }
-
-    void set(String name,int age,double gpa)
-    {
-        this.name=name;
-        this.age=age;
-        this.gpa=gpa;
-    }
-
-    String getname() {
-        return name;
-    }
-    int getage()
-    {
-        return age;
-    }
-    double getgpa()
-    {
-        return gpa;
-    }
-}
-class StudentTest
+class Studenttest
 {
     public static void main(String[] args)
     {
-        Student ob1 = new Student("kawin", 18, 7);
-        Student ob2 = new Student("kumar", 19, 20);
-        Student ob3 = new Student("kawin", 20, 9);
-        Student ob4 = new Student("kawin", 20, 25);
+        Logger l = Logger.getLogger("kawin");
+        Student object1 = new Student("kawin", 18, 7);
+        Student object2 = new Student("kumar", 19, 20);
+        Student object3 = new Student("mohan", 20, 9);
+        Student object4 = new Student("ragul", 20, 25);
 
         ArrayList <Student> list = new ArrayList<>();
-        list.add(ob1);
-        list.add(ob2);
-        list.add(ob3);
-        list.add(ob4);
+        list.add(object1);
+        list.add(object2);
+        list.add(object3);
+        list.add(object4);
+
+
 
         for(int i=0;i<list.size();i++)
         {
-            System.out.println(list.get(i).getname());
-            System.out.println(list.get(i).getage());
-            System.out.println(list.get(i).getgpa());
+            l.info(list.get(i).getname()+" "+list.get(i).getage()+" "+list.get(i).getgpa());
+
         }
+        l.info("after sorting");
+        Collections.sort(list, new Comparator<Student>() {
 
-        List <Double> list1 = new ArrayList<>();
-        for(int i=0;i<list.size();i++)
-        {
-            list1.add(list.get(i).getgpa());
-        }
-        Collections.sort(list1);
-        Collections.reverse(list1);
-
-
-        for(int i=0;i<list1.size();i++)
-        {
-            for(int m=i;m<list.size();m++)
-            {
-                if(list1.get(i) == list.get(m).getgpa())
-                {
-                    Collections.swap(list, i, m);
-                }
+            public int compare(Student o1, Student o2) {
+                return Double.compare(o2.getgpa(),o1.getgpa());
             }
-        }
+        });
+
+
+
         for(int i=0;i<list.size();i++)
         {
-            System.out.println(list.get(i).getname());
-            System.out.println(list.get(i).getage());
-            System.out.println(list.get(i).getgpa());
+            l.info(list.get(i).getname()+" "+list.get(i).getage()+" "+list.get(i).getgpa());
         }
     }
 }
